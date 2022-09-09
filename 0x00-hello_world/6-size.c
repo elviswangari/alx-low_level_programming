07875406d730f913ea78ad7f8e08824e673b7efa
elvis wangari <95751917+elviswangari@users.noreply.github.com>
2022-09-09 17:21:01 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,8 @@ int main(void)
 {
 	char acharacter;
 	int ainteger;
-	long along;long long alonglong;
+	long along;
+	long long alonglong;
 	float afloat;
 	
 	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
