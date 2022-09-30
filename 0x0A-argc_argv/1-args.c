0bf52c24e4d062178bd8758b9f61ce4973a42454
elvis wangare <elviswangare@gmail.com>
2022-09-30 19:27:33 +0000

    main function arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - program starts here
+ * @argc: argument count to the main function
+ * @argv: pointer to the command line inputs
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
