299b7095174476ac72dfa4b9d35aec49916f7fd4
elvis wangare <elviswangare@gmail.com>
2022-09-30 18:52:59 +0000

    arguments in main

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - program starts here
+* @argc: argument count to the function
+* @argv: pointer to the command line command
+* Return: 0
+*/
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define UTILS_UNUSED __attribute__((unused))
+#include <stdio.h>
+
+int _putchar(char c);
+#endif /*MAIN_H*/
