768a6ce3439a14019e03d2676ed82abafd541c02
elvis wangare <elviswangare@gmail.com>
2022-09-30 21:23:03 +0000

    multiply two numbers from chars

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,12 +9,19 @@
 int main(__attribute__((unused)) int argc, char *argv[])
 {
 	int mul, num1, num2;
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	mul = num1 * num2;
-	printf("%d\n", mul);
+	
+	if (argc -1 != 0)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mul = num1 * num2;
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 
 	return (0);
 }
-
