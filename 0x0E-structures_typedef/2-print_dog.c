4d5d5dc09656adffc4e1637c0d957073962f066e
elviswangare <elviswangare@gmail.com>
2022-10-11 19:18:25 +0000

    modified code

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,24 +1,20 @@
-#include "dog.h"
 #include <stdio.h>
+#include "dog.h"
 
 /**
- * print_dog - prints a struct dog.
- * @d: struct dog.
- *
- * Return: no return.
+ * print_dog - Print out all of the struct dog info
+ * @d: Pointer to struct
  */
 void print_dog(struct dog *d)
 {
-	if (d)
+	if (d != NULL)
 	{
-		if (!(d->name))
+		if (d->name == NULL)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
-
 		printf("Age: %f\n", d->age);
-
-		if (!(d->owner))
+		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
 		else
 			printf("Owner: %s\n", d->owner);
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,17 +2,15 @@
 #include <stdlib.h>
 
 /**
- * free_dog - frees dogs.
- * @d: struct dog.
- *
- * Return: no return.
+ * free_dog - Frees dog struct memory
+ * @d: dog to free
  */
 void free_dog(dog_t *d)
 {
-	if (d)
+	if (d != NULL)
 	{
-		free(d->name);
 		free(d->owner);
+		free(d->name);
 		free(d);
 	}
 }
