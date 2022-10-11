d222fb2b0ac7ecef4a838a9dab1f27a7cf59bb34
elviswangare <elviswangare@gmail.com>
2022-10-11 18:50:34 +0000

    structs and typedef

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+c structures and typedef are fun
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* dog_t - type or struct dog
+*/
+typedef struct dog dog_t;
+
+/**
+* struct dog - struct to store dog info
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+* Description: this struct stores name age and owner of the dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
